efd2b6635fb20262a4d5d2430d8ab49b8f0cc934
Neftalem <dneftalem@gmail.com>
2025-05-11 18:56:04 +0300

    add js open the nav bar and giv transform

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 
+const menuBtn = document.getElementById("menu-toggle");
+const sidebar = document.getElementById("sidebar");
 
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+  sidebar.classList.toggle("open");
+  menuBtn.textContent = menuBtn.classList.contains("open") ? "×" : "+";
+});
