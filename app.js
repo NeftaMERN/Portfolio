a9bfe4a2195a804f0bee0b07a8a8fbc9c97cb2ec
Neftalem <dneftalem@gmail.com>
2025-05-11 19:09:08 +0300

    remove the js code

--- app.js
+++
@@ -1,9 +0,0 @@
-
-const menuBtn = document.getElementById("menu-toggle");
-const sidebar = document.getElementById("sidebar");
-
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-  sidebar.classList.toggle("open");
-  menuBtn.textContent = menuBtn.classList.contains("open") ? "×" : "+";
-});
