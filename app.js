6666a6b48d424bd0a7e0c747ba163c4a95efd145
Neftalem <dneftalem@gmail.com>
2025-05-12 20:10:24 +0300

    add the js for side bar to opend

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const menu_toggle = document.getElementById("menu_toggle");
+const nav_btn = document.getElementById("nav_btn");
+
+ menu_toggle.onclick = function() {
+    nav_btn.classList.toggle("open");
+    menu_toggle.classList.toggle("rotate");
+    nav_btn.textContent.classList.contains("open") ? "×" : "+";
+ }
