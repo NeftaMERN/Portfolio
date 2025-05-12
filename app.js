e0488492f752762dd6e55d267da68dc1f1c23fea
Neftalem <dneftalem@gmail.com>
2025-05-12 20:21:12 +0300

    add the rotate &on sedebar_btn add the z-index

--- app.js
+++ app.js
@@ -5,5 +5,4 @@ const nav_btn = document.getElementById("nav_btn");
  menu_toggle.onclick = function() {
     nav_btn.classList.toggle("open");
     menu_toggle.classList.toggle("rotate");
-    nav_btn.textContent.classList.contains("open") ? "×" : "+";
  }
