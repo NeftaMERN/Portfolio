05d2c9c834cdd459458985e476f3f28e4d9926a9
Neftalem <dneftalem@gmail.com>
2025-05-12 20:30:00 +0300

    fix the side bar icons

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ const nav_btn = document.getElementById("nav_btn");
  menu_toggle.onclick = function() {
     nav_btn.classList.toggle("open");
     menu_toggle.classList.toggle("rotate");
+
+    if(menu_toggle.innerHTML === "+") {
+        menu_toggle.innerHTML = "×";
+    } else {
+        menu_toggle.innerHTML = "+";
+    }
  }
